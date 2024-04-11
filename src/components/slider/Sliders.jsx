5a6c47fbf9d273c419530b8c50ad2eba61713695
regone7 <regone12345@gmail.com>
2024-04-12 00:21:42 +0600

    slider add

---
+++ src/components/slider/Sliders.jsx
@@ -0,0 +1,56 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+
+// import required modules
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+
+const Sliders = () => {
+    return (
+        <div>
+            <Swiper
+                spaceBetween={30}
+                centeredSlides={true}
+                autoplay={{
+                    delay: 2000,
+                    disableOnInteraction: false,
+                }}
+                pagination={{
+                    clickable: true,
+                }}
+                navigation={true}
+                modules={[Autoplay, Pagination, Navigation]}
+                className="mySwiper"
+            >
+                <SwiperSlide>
+                    <div  >
+                        <img className='w-full bg-cover lg:h-[450px]  h-[250px]' src="https://i.ibb.co/0BJ0w9n/real-estate-6688945-1280.jpg" alt="" />
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div  >
+                        <img className='w-full bg-cover lg:h-[450px]  h-[250px]' src="https://i.ibb.co/9qK4RmF/realtor-3182376-1280.jpg" alt="" />
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div  >
+                        <img className='w-full bg-cover lg:h-[450px]  h-[250px]' src="https://i.ibb.co/2WLNfKR/house-1353389-1280.jpg" alt="" />
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div  >
+                        <img className='w-full bg-cover lg:h-[450px]  h-[250px]' src="https://i.ibb.co/LQ2tNzm/house-purchase-1019764-1280.jpg" alt="" />
+                    </div>
+                </SwiperSlide>
+
+
+            </Swiper>
+        </div>
+    );
+};
+
+export default Sliders;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,16 @@
+import Sliders from "../components/slider/Sliders";
 
 
 const Home = () => {
     return (
         <div>
-            home
+            <div className="container mx-auto ">
+                <Sliders></Sliders>
+            </div>
+            <div className="container mx-auto my-5">
+                <h1 className="text-2xl font-bold text-center my-3">Residential House</h1>
+                <p className="text-center ">Residential housing is often the largest and most important financial investment a person makes in their lifetime. Its value is determined by many factors, including size and location, and is subject to shifts in the market. </p>
+            </div>
         </div>
     );
 };
